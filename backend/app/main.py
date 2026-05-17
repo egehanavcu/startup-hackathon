394cc526eceed7ab5c882c620c9dca860b7a6ce5
Egehan <egehanavcu@gmail.com>
2026-05-17 08:28:15 +0300

    Production-ready

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.models import User, StudentTask, Class
 import socketio
@@ -10,14 +10,14 @@ origins = [
 
 Base.metadata.create_all(bind=engine)
 
-sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=origins)
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins=[])
 fastapi_app = FastAPI()
-fastapi_app.add_middleware(
-    CORSMiddleware,
+
+_socket_app = socketio.ASGIApp(sio, other_asgi_app=fastapi_app)
+asgi_app = CORSMiddleware(
+    _socket_app,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-asgi_app = socketio.ASGIApp(sio, other_asgi_app=fastapi_app)
